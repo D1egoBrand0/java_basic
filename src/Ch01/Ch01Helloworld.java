c6163bc6b38f4941ba6cb96451651d83cd2c19c0
IT KOREA <gjwlsgur123@gmail.com>
2022-12-29 16:57:01 +0900

    흐음

---
+++ src/Ch01/Ch01Helloworld.java
@@ -0,0 +1,20 @@
+package Ch01;  
+//패키지명(폴더와 유사)
+//';' : 문법의 마침표
+
+import java.util.*;
+
+public class Ch01Helloworld		//클래스영역 - 객체지향 문법 적용단위 
+{
+	
+	public static void main(String[] args)    //메서드 영역 - 절차지향문법 적용단위
+	{
+//		메서드란?(방법, 기능) : 작업수행을 하는데 필요한 여러코드들을 묶어서 실행하는데 사용되는 단위
+		System.out.println("HELLO WORLD!");
+		System.out.println("나는 행복합니다 이글스라 행복합니다");
+//		괄호가 붙으면 대부분 함수다
+//		main 메서드		: 최초 실행 함수. 운영체제와 연결되는 함수
+//		library 메서드	: 미리 만들어져 제공되는 함수.
+//		사용자 정의 메서드	: 개발자(본인)에 의해 생성하는 함수.
+	}
+}
