4e22706cf51a2d4ce89280f7e821b0a6dca9af6f
wearevenom <wearevenom@naver.com>
2022-12-30 18:40:04 +0900

    음수(+보수), 자료형 공부

--- src/Ch02/C01진수.java
+++ src/Ch02/C01진수.java
@@ -19,19 +19,34 @@ public class C01진수 {
 //		31 			00011111
 //		25			00011001
 //
-//		8bit단위로 계산하는 것이 좋다.
+//		8bit단위로 계산하는 것이 좋다.(1byte)
 		
 //		십진수를 2진수로
-//		int i = 25;
-//		String binaryString = Integer.toBinaryString(i); //2진수
-//		System.out.println(binaryString);
+		int i = 111;
+		String binaryString = Integer.toBinaryString(i); //2진수
+		System.out.println(binaryString);
 		
 
-		
-//		진법변호나 서식문자
+//		진법변환 서식문자
 //		%d		: 10진 정수 서식문자
 //		%o		: 8진수 서식문자
 //		%x		: 16진수 서식문자
+		
+		System.out.printf("10진수 : %d\n",0b1111);	//2진수 :2진수를 의미하는 접두사
+		System.out.printf("10진수 : %d\n",173);   	//10진수
+		System.out.printf("10진수 : %d\n",0255);  	//8진수 (0:8진수를 의미하는 접두사)
+		System.out.printf("10진수 : %d\n",0xAD);  	//16진수(0x:16진수를 의미하는 접두사)
+
+		System.out.printf("8진수 : %o\n",173);  		//16진수(0x:16진수를 의미하는 접두사)
+		System.out.printf("8진수 : %o\n",0255);  		//16진수(0x:16진수를 의미하는 접두사)
+		System.out.printf("8진수 : %o\n",0xAD);  		//16진수(0x:16진수를 의미하는 접두사)
+		// 16진수 표현 0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F		
+		System.out.printf("16진수 : %X\n",173);  		//16진수(0x:16진수를 의미하는 접두사)
+		System.out.printf("16진수 : %X\n",0255);  	//16진수(0x:16진수를 의미하는 접두사)
+		System.out.printf("16진수 : %X\n",0xAD);  	//16진수(0x:16진수를 의미하는 접두사)
+		
+		
+		
 		
 	}
 
