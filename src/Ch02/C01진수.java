17540ad771fffd06e47c2ce2214c8c38f15c3879
wearevenom <wearevenom@naver.com>
2022-12-30 17:24:26 +0900

    진수에 대하여

---
+++ src/Ch02/C01진수.java
@@ -0,0 +1,38 @@
+package Ch02;
+
+public class C01진수 {
+
+	public static void main(String[] args) {
+//		문제
+//		2진수를 10진수로
+//		01100010	64+32+2			98
+//		01010001	64+16+1			81
+//		01100110	64+32+4+2		102
+//		
+//		2진수를 10진수로
+//		String num2Str = "01100010";
+//		int num2 = Integer.parseInt(num2Str, 2);
+//		System.out.println(num2);
+		
+		
+//		10진수를 2진수로
+//		31 			00011111
+//		25			00011001
+//
+//		8bit단위로 계산하는 것이 좋다.
+		
+//		십진수를 2진수로
+//		int i = 25;
+//		String binaryString = Integer.toBinaryString(i); //2진수
+//		System.out.println(binaryString);
+		
+
+		
+//		진법변호나 서식문자
+//		%d		: 10진 정수 서식문자
+//		%o		: 8진수 서식문자
+//		%x		: 16진수 서식문자
+		
+	}
+
+}
