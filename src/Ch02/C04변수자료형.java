df93ae958b23131faa45088fc02a9cf2e1aa85a3
werarevenom <wearevenom@naver.com>
2023-01-02 16:43:40 +0900

    확인용

---
+++ src/Ch02/C04변수자료형.java
@@ -0,0 +1,15 @@
+package Ch02;
+
+public class C04변수자료형 {
+
+	public static void main(String[] args) {
+//		정수형 int-4byte 정수
+		int n1 = 0b10101101;		//2진수값
+		int n2 = 173;				//10진수
+		int n3 = 0255;				//8진수
+		int	n4 = 0xad;				//16진수
+		System.out.printf("%d %d %d %d\n", n1,n2,n3,n4);
+		
+	}
+
+}
